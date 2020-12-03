a6d5bb5f6c10440743d6ac7388069ff2f454cfeb
frank400 <j.victorhuguenin2018@gmail.com>
2020-12-02 21:32:25 -0300

    feat: add function to print matrix

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void lerMatriz(int mat[DIM][DIM])
+{
+    int i, j, p = 1;
+    for (i = 0; i < DIM; i++)
+    {
+        for (j = p; j < DIM; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+        p++;
+    }
+}
 void main(){
 
 
