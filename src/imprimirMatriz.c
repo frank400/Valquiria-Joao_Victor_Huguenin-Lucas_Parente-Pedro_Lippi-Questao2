cff47539545e777491ec6c161682d599b7945b38
Pedro Lippi <pedrokirby31@gmail.com>
2020-12-02 21:52:01 -0300

    feat: Criação de biblioteca para função ImprimirMatriz

--- src/imprimirMatriz.c
+++ src/imprimirMatriz.c
@@ -1,1 +1,13 @@
 #include "operacoes_matrix.h"
+
+void imprimirMatriz(int mat[DIM][DIM])
+{
+    for (int i = 0; i < DIM; i++)
+    {
+        for (int j = 0; j < DIM; j++)
+        {
+            printf("\t%d", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
