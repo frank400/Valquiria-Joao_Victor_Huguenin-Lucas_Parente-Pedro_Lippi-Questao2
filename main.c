a8a6959d271dc65a436fcdf09ef39d522b9c4e8b
frank400 <j.victorhuguenin2018@gmail.com>
2020-12-02 21:38:39 -0300

    feat: adiciona função para percorrer as cidades

--- main.c
+++ main.c
@@ -36,6 +36,74 @@ void imprimirMatriz(int mat[DIM][DIM])
     }
 }
 
+void percorrendoCidades(int mat[DIM][DIM], int x, int vet[DIM], int soma)
+{
+
+    int IndexMenorValor, k = 0, check, primeiro, soma_mil = 0;
+    vet[0] = x;
+    primeiro = x;
+    printf("\nPartindo da cidade %d:\n\n", primeiro);
+    while (k < DIM)
+    {
+        IndexMenorValor = x;
+
+        for (int i = 0; i < DIM; i++)
+        {
+
+            check = 0;
+            for (int y = 0; y < DIM; y++)
+            {
+
+                if (i == vet[y])
+                {
+                    check = 1;
+                }
+            }
+            if ((mat[x][i] < mat[x][IndexMenorValor] && check == 0) || (mat[x][IndexMenorValor] == 0 && check == 0))
+            {
+
+                IndexMenorValor = i;
+            }
+        }
+        soma = soma + mat[x][IndexMenorValor];
+        soma_mil = soma_mil + mat[x][IndexMenorValor];
+
+        for (int i = 1; i < DIM; i++)
+        {
+            if (vet[i] == -1)
+            {
+                vet[i] = IndexMenorValor;
+                break;
+            }
+        }
+        if (soma_mil > 1000)
+        {
+            printf("Pare na cidade %d, para nao chegar a %d quilometros em um dia\n\n", x, soma_mil);
+            soma_mil = mat[x][IndexMenorValor];
+        }
+        x = IndexMenorValor;
+
+        k++;
+    }
+
+
+    soma_mil = soma_mil + mat[x][primeiro];
+    if (soma_mil > 1000)
+    {
+        printf("Pare na cidade %d, para nao chegar a %d quilometros em um dia\n\n", x, soma_mil);
+        soma_mil = mat[x][IndexMenorValor];
+    }
+
+    for (int i = 0; i < DIM; i++)
+    {
+        printf("%d->", vet[i]);
+    }
+    printf("%d\n", vet[0]);
+    soma = soma + mat[x][primeiro];
+    printf("\n\n");
+    printf("Custo: %d\n", soma);
+}
+
 void main(){
 
 
