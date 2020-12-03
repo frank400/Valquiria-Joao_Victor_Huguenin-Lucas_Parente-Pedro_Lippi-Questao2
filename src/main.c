e2e05860bdc59764e044bb4990ed22a43111c8a0
frank400 <j.victorhuguenin2018@gmail.com>
2020-12-02 21:50:50 -0300

    feat: optimiza o uso das operações de matrix

--- src/main.c
+++ src/main.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "operacoes_matrix.h"
 
 #define DIM 30
-
-void lerMatriz(int mat[DIM][DIM]) {
-    int i, j, p = 1;
-    for (i = 0; i < DIM; i++) {
-        for (j = p; j < DIM; j++) {
-            scanf("%d", &mat[i][j]);
-        }
-        p++;
-    }
-}
-
-void espelhamentoMatriz(int mat[DIM][DIM]) {
-    for (int j = DIM - 1; j >= 0; j--) {
-        for (int i = DIM - 1; i >= 0; i--) {
-            mat[j][i] = mat[i][j];
-        }
-    }
-}
-
-void imprimirMatriz(int mat[DIM][DIM]) {
-    for (int i = 0; i < DIM; i++) {
-        for (int j = 0; j < DIM; j++) {
-            printf("\t%d", mat[i][j]);
-        }
-        printf("\n");
-    }
-}
 
 void percorrendoCidades(int mat[DIM][DIM], int x, int vet[DIM], int soma) {
 
