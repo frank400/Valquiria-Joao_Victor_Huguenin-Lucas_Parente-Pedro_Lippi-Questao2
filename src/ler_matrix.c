ea6ec2b2a9f7a942d6e41a47a5c8ba0ae85bc6fa
frank400 <j.victorhuguenin2018@gmail.com>
2020-12-02 21:48:01 -0300

    feat: implementa função para ler matrix na biblioteca operacoes_matrix

--- src/ler_matrix.c
+++ src/ler_matrix.c
@@ -1,1 +1,11 @@
 #include "operacoes_matrix.h"
+
+void lerMatriz(int mat[DIM][DIM]) {
+    int i, j, p = 1;
+    for (i = 0; i < DIM; i++) {
+        for (j = p; j < DIM; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+        p++;
+    }
+}
